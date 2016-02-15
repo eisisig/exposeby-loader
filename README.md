560984f0a495774761122a88d5e63380ab200dc8
Eisi Sig <eisi@meniga.is>
2016-02-15 12:55:35 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Expose By Loader
+
+## TODO
---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+ MIT License http://www.opensource.org/licenses/mit-license.php
+ Author Eisi Sig @eisisig
+ */
+
+var loaderUtils = require("loader-utils");
+
+module.exports = function () {};
+module.exports.pitch = function (remainingRequest) {
+	if ( !this.query ) throw new Error("query parameter is missing");
+	this.cacheable && this.cacheable();
+	var query = loaderUtils.parseQuery(this.query);
+	console.log('query.by', query.by);
+	var name = loaderUtils.interpolateName(this, query.by || "[name]", {});
+	var results = "";
+	results += "module.exports = " + "global[" + JSON.stringify(name) + "]";
+	return results + " = " +
+		"require(" + JSON.stringify("-!" + remainingRequest) + ");";
+};
